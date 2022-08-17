9ed728d745124cf06cb5c993347d21e86fac2868
bryanacolson <bryanacolson@gmail.com>
2022-08-16 20:21:18 -0500

    beau

--- scripts/group project 2.sql
+++ scripts/group project 2.sql
@@ -1,1 +1,6 @@
+-- app store
+select review_count, name, rating from app_store_apps where rating > 4.75;
+-- play
+select review_count, name, rating from play_store_apps where rating > 4.75;
+select review_count, name, rating from app_store_apps where review_count > 10000;
 
