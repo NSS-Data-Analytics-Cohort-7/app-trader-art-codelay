219056a225ca648f79d6dcb1f576154162796955
bryanacolson <bryanacolson@gmail.com>
2022-08-16 21:11:16 -0500

    beau

--- scripts/group project 2.sql
+++ scripts/group project 2.sql
@@ -2,5 +2,13 @@
 select review_count, name, rating from app_store_apps where rating > 4.75;
 -- play
 select review_count, name, rating from play_store_apps where rating > 4.75;
-select review_count, name, rating from app_store_apps where review_count > 10000;
-
+select price,review_count, name, rating from play_store_apps where rating > 4.75;
+SELECT count(a.name) as name, p.genres
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+GROUP BY p.genres
+ORDER BY name DESC
+Limit 10;
+select a.name, p.name
+(AVG(a.rating + p.rating)/2) as overall_avg_ratings, sum(casta.review_count as int) + p.review_count
