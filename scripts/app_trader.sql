c28abf4ed9d8f272795f050fdfbe76c6e8c23a9f
Tara-Nicole-Data <tara.nicole.data@gmail.com>
2022-08-18 19:45:29 -0500

    Working on grouping content ratings into 4 categories across both tables

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -1,10 +1,11 @@
 SELECT *
-FROM app_store_apps;
+FROM app_store_apps
+ORDER BY content_rating;
 
 SELECT *
 FROM play_store_apps;
 
---Top 10 Average ratings between both app stores with highest combined review counts
+--Top 10 Average ratings above 4.5 with highest combined review counts between both app stores
 
 SELECT a.name, p.name, 
     (AVG(a.rating + p.rating)/2) AS overall_avg_ratings, SUM(CAST(a.review_count AS int) + p.review_count) AS overall_review_counts
@@ -27,7 +28,7 @@ GROUP BY a.name, p.name
 ORDER BY overall_review_counts DESC
 LIMIT 10;
 
---From Tim / Drew
+--From Tim / Drew Top 10 genres
 
 SELECT COUNT(a.name) AS count, p.genres
 FROM app_store_apps AS a
@@ -37,3 +38,25 @@ GROUP BY p.genres
 ORDER BY count DESC
 LIMIT 10; 
 
+SELECT name, content_rating, rating, CAST(review_count AS int)
+FROM app_store_apps 
+ORDER BY rating DESC, review_count DESC, content_rating DESC
+LIMIT 10;
+
+SELECT name, content_rating, rating, CAST(review_count AS int)
+FROM play_store_apps
+ORDER BY rating DESC, review_count DESC, content_rating DESC
+LIMIT 10;
+
+SELECT content_rating, COUNT (content_rating), 
+    CASE
+        WHEN content_rating = '9+' THEN 'Pre-Teen+'
+        WHEN content_rating = '17+' THEN 'Adults Only'
+        WHEN content_rating = '12+' THEN 'Teen+'
+        WHEN content_rating = '4+' THEN 'Everyone'
+FROM app_store_apps
+GROUP BY content_rating
+UNION ALL
+SELECT content_rating, COUNT(content_rating)
+FROM play_store_apps
+GROUP BY content_rating; 
