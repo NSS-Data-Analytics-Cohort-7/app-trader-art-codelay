e3f776f4ef44463e89b3f8074b1def217b24c928
bryanacolson <bryanacolson@gmail.com>
2022-08-18 20:55:48 -0500

    top10_review_count

--- scripts/group project 2.sql
+++ scripts/group project 2.sql
@@ -19,6 +19,34 @@ GROUP BY p.price
 ORDER BY name DESC
 Limit 10;
 
-SELECT name, price, install_count, rating
+SELECT name, price, install_count, rating, review_count
 from play_store_apps
-where price Between and rating >='4.75' and install_count > '1000000';
+where price Between '.99' and '1.99' and rating >='4.75' and install_count>='10000000'
+order by install_count  DESC
+limit 10;
+
+--New Query
+
+
+--CHristian alsider
+ SELECT
+ name,
+ rating,
+ CAST(review_count as int)
+ FROM app_store_apps
+ WHERE CAST (review_count as int) > 750000
+ ORDER BY review_count DESC
+ limit 10;
+ 
+ SELECT
+ name,
+ rating,
+ CAST(review_count as int)
+ FROM app_store_apps
+ WHERE CAST (review_count as int) > 750000
+ ORDER BY review_count DESC
+ limit 10;
+ 
+
+
+ 
