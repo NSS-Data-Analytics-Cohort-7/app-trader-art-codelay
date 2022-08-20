9d8882e640f541d2470003f6396bc3976c8c4400
TJ Yost <tjyost@TJs-MacBook-Pro.local>
2022-08-20 10:57:24 -0500

    final 10 finished, working on presentation

--- Script/AppScript.sql
+++ Script/AppScript.sql
@@ -78,14 +78,7 @@ group by name, a_review_count
 order by a_review_count DESC
 
 -- im going to work on subqury these two
-
-Select * 
-from app_store_apps
-order by cast(review_count as int) DESC
-
-
-
-
+-- I wasnt able to add the two subquries, may be better to CTS them
 
 -- THis one was written by Drew, genres by count
 
@@ -119,6 +112,38 @@ Select name,
  (select (cast(size_bytes as int) / 1000000)
 from app_store_apps) as size_mb
 from app_store_apps
+
+
+
+
+-- This is my final Top 10
+
+
+SELECT DISTINCT(a.name), --drew distrinct names
+CAST(a.review_count AS int) AS a_review_count, --converts review count to int and aliased
+sum(p.review_count) AS p_review_count, --got the max review count and aliased
+sum(cast(a.review_count as int) + p.review_count) AS total_review_counts, -- added and cast review count as integer b/c its a text in app store, + review counts from play store as alias,
+a.rating AS a_rating, -- rating from apple
+p.rating AS p_rating, --rating from play store
+p.genres, --genres from play
+a.primary_genre, -- genres from apple
+cast(a.price as money) -- cast price from apple as money to show dollar amount
+FROM app_store_apps AS a --aliased apple table
+INNER JOIN play_store_apps AS p -- used inner joined play to collect only same collums
+ON a.name = p.name -- matched on name from both
+WHERE a.price <= 1 AND a.rating >= 4.5 AND p.rating >= 4.5 -- filterd by price equal and less than 1 and where rating greated and equal to 4.5
+GROUP BY a.review_count, a.rating, p.rating, a.name,  a.primary_genre, p.genres,a.price
+ORDER BY total_review_counts DESC --orded by total review in desc order
+Limit 10; --top 10
+
+
+
+
+
+
+
+
+
 
 
 
